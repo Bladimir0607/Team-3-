4e3377b5f7da7b2b740cd8b6c138a3883b4b1902
Bladimir <vladimirantonioventura6@gmail.com>
2025-03-04 18:50:11 -0400

    actualizacion

---
+++ Preguntas historicas project two.py
@@ -0,0 +1,10 @@
+while True:
+    
+
+
+    #Inicio de preguntas historicas 
+    WelcometoHistory = input("Buenas tardes, Bienvenido/a , a preguntas Historicas.")
+    Name= input("Ingresa tu nombre : ")
+    edad = input ("Ingresa tu edad :")
+    Como_estas = input ("¿Como Estás") 
+    Nuestro_viaje = input ("Iniciamos nuestro viaje , pulse eter para continuar...")
---
+++ pseudocodigo.py
@@ -0,0 +1,21 @@
+# Inicio# 
+
+# Pedir al usuario que ingrese dos números# 
+
+num1 = int (input ("Ingrese el primer número: ")) 
+
+num2 = int (input ("Ingrese el segundo número: ")) 
+
+  
+
+# Sumar los dos números# 
+
+suma = num1 + num2 
+
+  
+
+# Mostrar el resultado# 
+
+print ("La suma de los dos números es:", suma) 
+
+# Fin# 
